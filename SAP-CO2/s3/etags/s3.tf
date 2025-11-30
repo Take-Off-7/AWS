4a294e7cb4dab90e9cbbb95a05879110f309e6bf
takeoff_7 <tfakeye7@gmail.com>
2025-11-30 13:54:22 +0000

    refactor bash script and create etags proj

---
+++ SAP-CO2/s3/etags/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
+}
